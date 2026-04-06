441edfa2fc760447270ad139e91d32267e130944
OmGhayal3114 <omghayal3114@gmail.com>
2026-04-06 22:05:25 +0530

    Update Libray.c

--- Libray.c
+++ Libray.c
@@ -4,7 +4,7 @@
 #define N 100
 typedef struct node
 {
-    char name[N];
+    char name[N],author[N];
     int id,no;            // nook ID and no. of avability
     float price;
     struct node *next;
@@ -13,11 +13,12 @@ typedef struct LL
 {
     node *start;
 }LL;
-void insert(LL *l,char name[N],int id,float price,int no)
+void insert(LL *l,int id,char name[N],char auth[N],float price,int no)
 {
     node *p,*newrec;
     newrec = (node*)malloc(sizeof(node));
     strcpy(newrec->name,name);
+    strcpy(newrec->author,auth);
     newrec->id=id;
     newrec->price=price;
     newrec->no=no;
@@ -49,8 +50,7 @@ void delete(LL *l)
         {
             if(l->start==p)
             {
-                q=l->start;
-                l->start=NULL;
+                l->start=p->next;
             }
             else{
                 q->next=p->next;
@@ -69,27 +69,43 @@ void delete(LL *l)
     printf("Book is not found");
     
 }
-void sort(LL*l)
-{
-    node*i,*j;
-     node temp;
-    if(l->start==NULL)
-    {
-        printf("\nNo nodes to sort");
-        return;
-    }
-    for(i=l->start;i->next!=NULL;i=i->next)
-        {
-            for(j=l->start;j->next!=NULL;j=j->next)
-                {
-                    if(j->id > j->next->id)
-                    {
-                        temp=*j;
-                        *j=*(j->next);
-                        *(j->next)=temp;
-                    }
-                }
+void sort(LL *l) {
+    node *i, *j;
+    int t_id, t_no;
+    float t_pr;
+    char t_nm[N], t_au[N];
+
+    if (l->start == NULL) return;
+
+    for (i = l->start; i != NULL; i = i->next) {
+        for (j = l->start; j->next != NULL; j = j->next) {
+            if (j->id > j->next->id) {
+                // Swap only data fields
+                t_id = j->id; j->id = j->next->id; j->next->id = t_id;
+                t_no = j->no; j->no = j->next->no; j->next->no = t_no;
+                t_pr = j->price; j->price = j->next->price; j->next->price = t_pr;
+                strcpy(t_nm, j->name); strcpy(j->name, j->next->name); strcpy(j->next->name, t_nm);
+                strcpy(t_au, j->author); strcpy(j->author, j->next->author); strcpy(j->next->author, t_au);
+            }
         }
+    }
+}
+
+void borrow(LL *l, char name[N]) {
+    node *p = l->start; 
+    while (p != NULL) {
+        if (strcasecmp(p->name, name) == 0) {
+            if (p->no > 0) {
+                p->no--;
+                printf("\nBook successfully borrowed! Remaining: %d\n", p->no);
+            } else {
+                printf("\nOut of stock!\n");
+            }
+            return;
+        }
+        p = p->next;
+    }
+    printf("\nBook not found!\n");
 }
 int search(LL *l, char a[])
 {  
@@ -98,10 +114,9 @@ int search(LL *l, char a[])
 
     while (p != NULL)
     {
-        if (strcasecmp(p->name, a) == 0 && (p->no > 0))
+        if (strcasecmp(p->name, a) == 0)
         {
-            printf("\nBook details:\nBook name: %s\tBook id: %d\tNumber of copies: %d\n",
-                   p->name, p->id, p->no);  
+            printf("\nBook details:\nBook name: %s\nAuthor Name : %s\nBook id: %d\nNumber of copies: %d\n",p->name, p->id, p->no);  
             return 1;             // Book found
         }
         else
@@ -112,18 +127,8 @@ int search(LL *l, char a[])
 
     return 0;             // book not found
 }
-void borrow(LL *l,char name[N])
-{
-    node *p;
-    p=l->start;
-    while(strcasecmp(p->name,name)!=0)
-    {
-        p=p->next;
-    }
-    p->no--;
-    printf("Book is Succesfully Purchased");
-}
-void returnback(LL *l, char a[], int id)
+
+void returnback(LL *l, char a[])
 {
     node *p = l->start;
     while (p != NULL)
@@ -137,4 +142,104 @@ void returnback(LL *l, char a[], int id)
         p = p->next;
     }
     printf("\nThis book is not from library");
+}
+void display(LL *l)
+{
+    node *p;
+    p=l->start;
+    printf("------------------------------------------------------------------------\n");
+    printf("%-5s %-25s %-25s %-10s %-10s\n", "ID", "Name", "Author", "Price", "Qty");
+    while(p!=NULL)
+    {
+        printf("%-5d %-25s %-25s %-10f %-10d\n",p->id,p->name,p->author,p->price,p->no);
+        p=p->next;
+    }
+
+    printf("------------------------------------------------------------------------");
+}
+int main()
+{
+    int ch,x,q,id;
+    float p;
+    char n[N],a[N];
+    LL l;
+    l.start=NULL;
+    insert(&l,101,"The Great Gatsby","F. Scott Fitzgerald",15.99,12);
+    insert(&l,105,"C Programming","Dennis Ritchie",45.00,6);
+    insert(&l,103,"To Kill a Mockingbird","Harper Lee",18.25,3);
+    insert(&l,102,"1984","George Orwell",12.50,9);
+    insert(&l,104,"The Hobbit","J.R.R. Tolkien",20.00,10);
+    display(&l);
+    while(1){
+        
+        printf("\nMenu\n1-Insert a New Book\n2-Remove Book\n3-Barrow a Book\n4-Return Book\n5-search Book\n6-Display Details of Book\n7-Exit\nEnter Choice:");
+    
+        scanf("%d",&ch);
+        // scanf(" %[^\n]",n);
+        if(ch==7)
+        break;
+
+        switch (ch)
+        {
+        case 1:
+            {
+                printf("Enter Details\nEnter Name:");
+                scanf(" %[^\n]",n);
+                printf("Enter Author Name : ");
+                scanf(" %[^\n]",a);
+                printf("Enter ID of Book: ");
+                scanf("%d",&id);
+                printf("Enter Price of Book: ");
+                scanf("%f",&p);
+                printf("Enter Quantity of Book: ");
+                scanf("%d",&q);
+                insert(&l,id,n,a,p,q);
+                sort(&l);
+                printf("Book is Inserted Succefully!");
+            }
+            break;
+        case 2:
+        {
+            delete(&l);
+        }
+        break;
+        case 3:{
+            printf("Enter Name of Book you want to Borrow :");
+            scanf(" %[^\n]",n);
+            if(search(&l,n))
+            {
+                borrow(&l,n);
+            }
+        }
+        break;
+        case 4:
+        {
+            printf("Enter a Name of Book you want to Return: ");
+            scanf(" %[^\n]",n);
+            returnback(&l,n);
+        }
+        break;
+        case 5:{
+             printf("Enter a Name of Book you want to Search: ");
+            scanf(" %[^\n]",n);
+            if(search(&l,n))
+            {
+                printf("\n1-Borrow the Book\n2-Return the book\nEnter Choice : ");
+                scanf("%d",&x);
+                
+                if(x==1)
+                borrow(&l,n);
+                else
+                returnback(&l,n);
+            }
+        }
+        break;
+        case 6:display(&l);
+        break;
+        default:printf("Invalid Choice");
+            break;
+        }
+        
+    }
+    return 0;
 }
